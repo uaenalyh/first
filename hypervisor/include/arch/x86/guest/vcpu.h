fyin1/acrn-hypervisor

hv: Add function to set UOS BSP init state
NOTE: this patch is only workaround patch for UOS BSP state init.
Eventually, the DM will call hypercall to init UOS BSP state.

We use this workaround patch here to simplify the init_guest_state.
Will make the caller of init_guest_state calls init_guest_vmx
directly.

Tracked-On: #1231
Signed-off-by: Yin Fengwei <fengwei.yin@intel.com>

hypervisor/include/arch/x86/guest/vcpu.h
@@ -288,6 +288,8 @@ void vcpu_set_pat_ext(struct vcpu *vcpu, uint64_t val);
 void set_vcpu_regs(struct vcpu *vcpu, struct acrn_vcpu_regs *vcpu_regs);
 void reset_vcpu_regs(struct vcpu *vcpu);
 void set_ap_entry(struct vcpu *vcpu, uint64_t entry);
+void set_bsp_rm_entry(struct vcpu *vcpu);
+void set_bsp_protect_regs(struct vcpu *vcpu);
 
 struct vcpu* get_ever_run_vcpu(uint16_t pcpu_id);
 int create_vcpu(uint16_t pcpu_id, struct vm *vm, struct vcpu **rtn_vcpu_handle);

uaenalyh/first

Acked-by: Eddie Dong <eddie.dong@intel.com>

hypervisor/include/arch/x86/guest/vcpu.h
@@ -288,6 +288,8 @@ void vcpu_set_pat_ext(struct vcpu *vcpu, uint64_t val);
 void set_vcpu_regs(struct vcpu *vcpu, struct acrn_vcpu_regs *vcpu_regs);
 void reset_vcpu_regs(struct vcpu *vcpu);
 void set_ap_entry(struct vcpu *vcpu, uint64_t entry);
+void set_bsp_real_mode_entry(struct vcpu *vcpu);
+void set_bsp_protect_mode_regs(struct vcpu *vcpu);
 
 static inline bool is_long_mode(struct vcpu *vcpu)
 {